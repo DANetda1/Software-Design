00d056cb832046ac72f702ab45bbde7b0703aa80
Abdul Magomedov <166478721+DANetda1@users.noreply.github.com>
2025-02-14 23:45:27 +0300

    Add files via upload

---
+++ MiniHWs/HW1/ZooApplication/ZooApplication/Animals/Monkey.cs
@@ -0,0 +1,10 @@
+﻿namespace ZooApplication.Models.Animals
+{
+    public class Monkey : Herbo
+    {
+        public Monkey(int id, string name, int food, int kindnessLevel)
+            : base(id, name, food, kindnessLevel)
+        {
+        }
+    }
+}
---
+++ MiniHWs/HW1/ZooApplication/ZooApplication/Services/VeterinaryClinic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ZooApplication.Services
+{
+    public class VeterinaryClinic
+    {
+        public bool CheckHealth(string animalName)
+        {
+            Console.WriteLine($"Проверка здоровья животного \"{animalName}\"...");
+            // Проверка 50 на 50
+            return new Random().Next(2) == 1;
+        }
+    }
+}
